f08b579bd389820242d6f7e47ed142e6567d647d
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2023-10-19 12:14:01 +0200

    feat(ci): add sentry User Feedback widget

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -27,4 +27,12 @@ Sentry.init({
       blockAllMedia: true,
     }),
   ],
+
+  beforeSend(event, hint) {
+    // Check if it is an exception, and if so, show the report dialog
+    if (event.exception) {
+      Sentry.showReportDialog({ eventId: event.event_id })
+    }
+    return event
+  },
 })
