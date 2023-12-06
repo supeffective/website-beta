9d474584715d2058b5796e3d9bf430a1bbedf6ec
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2023-12-06 14:46:33 +0100

    wip: link account in profile editor

--- components/user/UserProfileEditor.tsx
+++ components/user/UserProfileEditor.tsx
@@ -1,6 +1,7 @@
 import { UserRecord } from '@/lib/users/types'
 import Link from 'next/link'
 import { Suspense } from 'react'
+import { SignInForm } from '../layout/signin-form'
 import { Button } from '../ui/button'
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '../ui/card'
 import { AvatarSelector } from './AvatarSelector'
@@ -12,31 +13,36 @@ type UserProfileEditorProps = {
 export function UserProfileEditor(props: UserProfileEditorProps) {
   const { user } = props
   return (
-    <Card className="flex-1">
-      <CardHeader>
-        <CardTitle>Edit Profile</CardTitle>
-        <CardDescription>Editing user profile for {user.name}</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <p>Choose an avatar:</p>
-        <Suspense
-          fallback={
-            <div>
-              <p>Loading avatars...</p>
-            </div>
-          }
-        >
-          <AvatarSelector />
-        </Suspense>
-      </CardContent>
-      <CardFooter className="justify-end gap-2">
-        <Button variant="secondary" asChild>
-          <Link href="/profile">Save</Link>
-        </Button>
-        <Button variant="primary" asChild>
-          <Link href="/profile">Cancel</Link>
-        </Button>
-      </CardFooter>
-    </Card>
+    <>
+      <Card className="flex-1">
+        <CardHeader>
+          <CardTitle>Edit Profile</CardTitle>
+          <CardDescription>Editing user profile for {user.name}</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Choose an avatar:</p>
+          <Suspense
+            fallback={
+              <div>
+                <p>Loading avatars...</p>
+              </div>
+            }
+          >
+            <AvatarSelector />
+          </Suspense>
+        </CardContent>
+        <CardFooter className="justify-end gap-2">
+          <Button variant="secondary" asChild>
+            <Link href="/profile">Save</Link>
+          </Button>
+          <Button variant="primary" asChild>
+            <Link href="/profile">Cancel</Link>
+          </Button>
+        </CardFooter>
+      </Card>
+
+      <h3>Link an account:</h3>
+      <SignInForm />
+    </>
   )
 }
